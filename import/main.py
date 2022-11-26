97e0a8c609f8703e64d3e2bcf5add2e69e613572
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2022-11-26 17:50:22 +0200

    import.

--- import/main.py
+++ import/main.py
@@ -1,5 +1,7 @@
 import message
 
+print('main.py')
+
 if __name__ == '__main__':
     print(__name__)
     print(message.__name__)
--- import/message.py
+++ import/message.py
@@ -1,2 +1,4 @@
+print('message.py')
+
 if __name__ == '__main__':
-    print(__name__)
+    print('--', __name__)
